b0c397e93dca2cb44647c1a0978e6ba6e1c4a5a2
Zubair Ibn Zamir <zizdjbd@gmail.com>
2025-04-23 12:09:36 +0600

    astro:build:done

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import { defineConfig } from 'astro/config';
 import remarkToc from 'remark-toc'
 import remarkSlug from 'remark-slug'
 import react from '@astrojs/react';
-// import pagefind from "astro-pagefind";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-    // build: {
-    //     format: "file",
-    //   },
-    integrations:[react()],
+    build: {
+        format: "file",
+      },
+    integrations:[react(), pagefind()],
     markdown: {
         remarkPlugins: [remarkToc],
     }
